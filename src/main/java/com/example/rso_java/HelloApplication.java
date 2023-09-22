86987178e864de984202eff1b8b52aff1063472a
K06-DM-09 <dan.stumberger.x@gmail.com>
2023-09-22 13:10:45 +0200

    Bla bla

---
+++ src/main/java/com/example/rso_java/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.example.rso_java;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Group root=new Group();
+        Scene scene = new Scene(root, 800, 600);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
